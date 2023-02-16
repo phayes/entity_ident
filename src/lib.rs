8013871c9253abbf7c93bfc2019a526bcc01c11a
Patrick Hayes <patrick.d.hayes@gmail.com>
2023-02-16 09:28:42 -0800

    Adding in def_id_serde_impls via conditional compilation

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,7 @@ macro_rules! def_id {
             }
 
             /// The prefix of the id (e.g. `cus` for a `CustomerId`).
+            #[allow(dead_code)]
             #[inline(always)]
             pub fn inner(&self) -> &$crate::Identifier {
                 &self.0
@@ -127,123 +128,9 @@ macro_rules! def_id {
                 }
             }
         }
-    };
-    (#[optional] enum $enum_name:ident { $( $variant_name:ident($($variant_type:tt)*) ),* $(,)* }) => {
-        #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
-        pub enum $enum_name {
-            $( $variant_name($($variant_type)*), )*
-        }
 
-        impl $enum_name {
-            pub fn as_str(&self) -> &str {
-                match *self {
-                    $( $enum_name::$variant_name(ref id) => id.as_str(), )*
-                }
-            }
-
-            pub fn as_bytes(&self) -> &str {
-                match *self {
-                    $( $enum_name::$variant_name(ref id) => id.as_bytes(), )*
-                }
-            }
-
-            pub fn inner(&self) -> &Identifier {
-                match *self {
-                    $( $enum_name::$variant_name(ref id) => id.inner(), )*
-                }
-            }
-        }
-
-        impl PartialEq<str> for $enum_name {
-            fn eq(&self, other: &str) -> bool {
-                self.as_str() == other
-            }
-        }
-
-        impl PartialEq<&str> for $enum_name {
-            fn eq(&self, other: &&str) -> bool {
-                self.as_str() == *other
-            }
-        }
-
-        impl PartialEq<String> for $enum_name {
-            fn eq(&self, other: &String) -> bool {
-                self.as_str() == other
-            }
-        }
-
-        impl AsRef<str> for $enum_name {
-            fn as_ref(&self) -> &str {
-                self.as_str()
-            }
-        }
-
-        impl std::ops::Deref for $enum_name {
-            type Target = str;
-
-            fn deref(&self) -> &str {
-                self.as_str()
-            }
-        }
-
-        impl PartialOrd for $enum_name {
-            fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-                Some(self.cmp(other))
-            }
-        }
-
-        impl Ord for $enum_name {
-            fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-                self.as_str().cmp(other.as_str())
-            }
-        }
-
-        impl std::fmt::Display for $enum_name {
-            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                match *self {
-                    $( $enum_name::$variant_name(ref id) => id.fmt(f), )*
-                }
-            }
-        }
-
-        impl std::str::FromStr for $enum_name {
-            type Err = ParseIdError;
-
-            fn from_str(s: &str) -> Result<Self, Self::Err> {
-                let prefix = s.find('_')
-                    .map(|i| &s[0..=i])
-                    .ok_or_else(|| ParseIdError {
-                        typename: stringify!($enum_name),
-                        expected: "id to start with a prefix (as in 'prefix_')"
-                    })?;
-
-                match prefix {
-                    $(_ if $($variant_type)*::is_valid_prefix(prefix) => {
-                        Ok($enum_name::$variant_name(s.parse()?))
-                    })*
-                    _ => {
-                        Err(ParseIdError {
-                            typename: stringify!($enum_name),
-                            expected: "unknown id prefix",
-                        })
-                    }
-                }
-            }
-        }
-
-        $(
-            impl From<$($variant_type)*> for $enum_name {
-                fn from(id: $($variant_type)*) -> Self {
-                    $enum_name::$variant_name(id)
-                }
-            }
-
-            impl PartialEq<$($variant_type)*> for $enum_name {
-                fn eq(&self, other: &$($variant_type)* ) -> bool {
-                    self.as_str() == other.as_str()
-                }
-            }
-        )*
+        // Injected via build.rs for serde support
+        def_id_serde_impls!($struct_name);
     };
     (enum $enum_name:ident { $( $(#[$test:meta])? $variant_name:ident($($variant_type:tt)*) ),+ $(,)? }) => {
         #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -258,12 +145,14 @@ macro_rules! def_id {
                 }
             }
 
+            #[allow(dead_code)]
             pub fn as_bytes(&self) -> &[u8] {
                 match *self {
                     $( $enum_name::$variant_name(ref id) => id.as_bytes(), )*
                 }
             }
 
+            #[allow(dead_code)]
             pub fn inner(&self) -> &Identifier {
                 match *self {
                     $( $enum_name::$variant_name(ref id) => id.inner(), )*
@@ -349,10 +238,16 @@ macro_rules! def_id {
                 }
             }
         )*
+
+        // Injected via build.rs for serde support
+        def_id_serde_impls!($enum_name);
     };
 }
 
+//include!(concat!(env!("OUT_DIR"), "/serde_impl.rs"));
+
 #[cfg(feature = "serde")]
+#[macro_export]
 macro_rules! def_id_serde_impls {
     ($struct_name:ident) => {
         impl serde::Serialize for $struct_name {
@@ -377,48 +272,12 @@ macro_rules! def_id_serde_impls {
     ($struct_name:ident, _) => {};
 }
 
-#[cfg(feature = "serde")]
-macro_rules! def_id {
-    ($struct_name:ident, $prefix:literal $(| $alt_prefix:literal)* $(, { $generate_hint:tt })?) => {
-        def_id_serde_impls!($struct_name $(, $generate_hint )*);
-    };
-    (#[optional] enum $enum_name:ident { $( $variant_name:ident($($variant_type:tt)*) ),* $(,)* }) => {
-
-        impl serde::Serialize for $enum_name {
-            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                where S: serde::ser::Serializer
-            {
-                self.as_str().serialize(serializer)
-            }
-        }
-
-        impl<'de> serde::Deserialize<'de> for $enum_name {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where D: serde::de::Deserializer<'de>
-            {
-                let s: String = serde::Deserialize::deserialize(deserializer)?;
-                s.parse::<Self>().map_err(::serde::de::Error::custom)
-            }
-        }
-    };
-    (enum $enum_name:ident { $( $(#[$test:meta])? $variant_name:ident($($variant_type:tt)*) ),+ $(,)? }) => {
-        impl serde::Serialize for $enum_name {
-            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                where S: serde::ser::Serializer
-            {
-                self.as_str().serialize(serializer)
-            }
-        }
-
-        impl<'de> serde::Deserialize<'de> for $enum_name {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where D: serde::de::Deserializer<'de>
-            {
-                let s: String = serde::Deserialize::deserialize(deserializer)?;
-                s.parse::<Self>().map_err(::serde::de::Error::custom)
-            }
-        }
-    };
+#[cfg(not(feature = "serde"))]
+#[doc(hidden)]
+#[macro_export]
+macro_rules! def_id_serde_impls {
+    ($struct_name:ident) => {};
+    ($struct_name:ident, _) => {};
 }
 
 
